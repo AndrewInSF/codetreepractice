8b77f01e919e9af369c5b533ae18a6385feef1c8
AndrewInSF <113637158+AndrewInSF@users.noreply.github.com>
2026-06-14 12:18:48 -0700

    [Passed] 100점 도달하기 (127ms, 10MB)

---
+++ trail1/100점 도달하기/reach-100-point.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int score =sc.nextInt();
+
+
+        for(int x = score ; x <= 100; x++)
+        {
+            if(x >= 90)
+            {
+                System.out.print('A' + " ");
+            }else if(x >= 80)
+            {
+                System.out.print('B' + " ");
+
+
+            }else if(x >= 70)
+            {
+                System.out.print('C' + " ");
+
+            }else if(x >= 60)
+            {
+                System.out.print('D' + " ");
+
+            }else{
+                System.out.print('F' + " ");
+
+            }
+            
+        }
+    }
+}
